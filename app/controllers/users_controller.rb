b466771ae9619b237b1ead1c68a6e4bfb2dfcee5
George <george.gbenle@outlook.com>
2021-02-17 19:22:21 +0000

    Set up params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
     def create
             # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+            @user = User.new(user_params)
 
             if @user.save
               redirect_to new_user_path
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     end
 
     private
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
