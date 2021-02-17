cbc1e6ebe0fe8e64c5327c09ea9790c91ab87ae3
George <george.gbenle@outlook.com>
2021-02-17 16:35:02 +0000

    edit creat action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
     end
 
     def create
+        def create
+            @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+          
+            if @user.save
+              redirect_to new_user_path
+            else
+              render :new
+            end
+          end
     end
 end
