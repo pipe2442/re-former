dd6a23cc7f51f6dca7d38b96ae972caeaaf5430e
George <george.gbenle@outlook.com>
2021-02-18 02:46:17 +0000

    fix rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
-    def new
-      @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def edit
-        @user = User.find_by(id:params[:id])
-    end
+  private
 
-    def create
-            # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-            @user = User.new(user_params)
-
-            if @user.save
-              redirect_to new_user_path
-            else
-              render :new
-            end
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
-  get "/users/edit"
-  post "/users/edit"
-  root "users#new"
+  resources :users, only: %i[new create]
+  get '/users/edit'
+  post '/users/edit'
+  root 'users#new'
 end
