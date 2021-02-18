1d9202e60444c0877c241561b877299b9e20968a
George <george.gbenle@outlook.com>
2021-02-18 00:26:55 +0000

    Create edit post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
   get "/users/edit"
-  
+  post "/users/edit"
   root "users#new"
 end
