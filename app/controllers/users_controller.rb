e14176e148ee611647d973996925dde7a38be2ff
George <george.gbenle@outlook.com>
2021-02-18 13:17:43 +0000

    Create update route, update controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def create
     @user = User.new(user_params)
 
@@ -14,6 +10,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
